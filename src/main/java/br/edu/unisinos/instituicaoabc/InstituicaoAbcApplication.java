3fc032c51126432c69d009a8a1a962be4c340d4f
Leandro Kersting de Freitas <leandro@frekele.org>
2022-10-07 23:46:10 -0300

    init

---
+++ src/main/java/br/edu/unisinos/instituicaoabc/InstituicaoAbcApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unisinos.instituicaoabc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstituicaoAbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstituicaoAbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/unisinos/instituicaoabc/InstituicaoAbcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unisinos.instituicaoabc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstituicaoAbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
